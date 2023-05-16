ee9c74cb81db3207a1baf5d24fe0a03b4cd0e207
Yash Sanjeev Halwai <halwaiyash45@gmail.com>
2023-05-16 08:45:22 +0530

    1.0

---
+++ fooditem.js
@@ -0,0 +1,249 @@
+const foodItem= [
+{
+    id: 1,
+    name: 'Shev Bhaji',
+    category : 'pureveg',
+    price: 130,
+    img: '/images/pureveg/shev-bhaji.webp',
+    quantity: 1
+},
+{
+    id: 2,
+    name: 'Mix Veg',
+    category : 'pureveg',
+    price: 180,
+    img: '/images/pureveg/Mix-Veg.jpg',
+    quantity: 1
+},
+{
+    id: 3,
+    name: 'Veg Kofta',
+    category : 'pureveg',
+    price: 200,
+    img: '/images/pureveg/Veg-Kofta.jpg',
+    quantity: 1
+},
+{
+    id: 4,
+    name: 'Veg Kolhapuri',
+    category : 'pureveg',
+    price: 190,
+    img: '/images/pureveg/veg-kolhapuri.jpg',
+    quantity: 1
+},
+{
+    id: 5,
+    name: 'Veg Patiala',
+    category : 'pureveg',
+    price: 220,
+    img: '/images/pureveg/veg-patiala.jpg',
+    quantity: 1
+},
+
+{
+    id: 6,
+    name: 'Chilli Paneer',
+    category : 'paneer',
+    price: 230,
+    img: '/images/paneer/chilli-paneer.jpg',
+    quantity: 1
+},
+{
+    id: 7,
+    name: 'Matar Paneer',
+    category : 'paneer',
+    price: 240,
+    img: '/images/paneer/Matar-Paneer.jpg',
+    quantity: 1
+},
+{
+    id: 8,
+    name: 'Butter Paneer',
+    category : 'paneer',
+    price: 225,
+    img: '/images/paneer/paneer-butter-masala.jpg',
+    quantity: 1
+},
+{
+    id: 9,
+    name: 'Paneer Hyderabadi',
+    category : 'paneer',
+    price: 245,
+    img: '/images/paneer/Paneer-Hyderabadi.jpg',
+    quantity: 1
+},
+{
+    id: 10,
+    name: 'Shahi Paneer',
+    category : 'paneer',
+    price: 237,
+    img: '/images/paneer/shahi-paneer.jpg',
+    quantity: 1
+},
+
+{
+    id: 11,
+    name: 'Steam Rice',
+    category : 'rice',
+    price: 110,
+    img: '/images/rice/Steam-rice.jpg',
+    quantity: 1
+},
+{
+    id: 12,
+    name: 'Jeera Rice',
+    category : 'rice',
+    price: 135,
+    img: '/images/rice/Jeera-Rice.jpg',
+    quantity: 1
+},
+{
+    id: 13,
+    name: 'Masala Khichdi',
+    category : 'rice',
+    price: 155,
+    img: '/images/rice/masala-khichdi.jpg',
+    quantity: 1
+},
+{
+    id: 14,
+    name: 'Vegetable Biryani',
+    category : 'rice',
+    price: 162,
+    img: '/images/rice/Vegetable-Biryani.jpg',
+    quantity: 1
+},
+{
+    id: 15,
+    name: 'Chicken Biryani',
+    category : 'rice',
+    price: 250,
+    img: '/images/rice/Chicken-Biryani.jpg',
+    quantity: 1
+},
+
+{
+    id: 16,
+    name: 'French Fries',
+    category : 'chinese',
+    price: 85,
+    img: '/images/chinese/French-Fries.jpg',
+    quantity: 1
+},
+{
+    id: 17,
+    name: 'Manchurian',
+    category : 'chinese',
+    price: 100,
+    img: '/images/chinese/manchurian.jpg',
+    quantity: 1
+},
+{
+    id: 18,
+    name: 'Momos',
+    category : 'chinese',
+    price: 120,
+    img: '/images/chinese/momos.jpg',
+    quantity: 1
+},
+{
+    id: 19,
+    name: 'Noodles',
+    category : 'chinese',
+    price: 65,
+    img: '/images/chinese/noodles.jpeg',
+    quantity: 1
+},
+{
+    id: 20,
+    name: 'Spring Rolls',
+    category : 'chinese',
+    price: 60,
+    img: '/images/chinese/spring-rolls.jpg',
+    quantity: 1
+},
+
+{
+    id: 21,
+    name: 'Idli',
+    category : 'southindian',
+    price: 75,
+    img: '/images/southindian/idli.jpg',
+    quantity: 1
+},
+{
+    id: 22,
+    name: 'Masala Dosa',
+    category : 'southindian',
+    price: 120,
+    img: '/images/southindian/masala-dosa.jpg',
+    quantity: 1
+},
+{
+    id: 23,
+    name: 'Plain Dosa',
+    category : 'southindian',
+    price: 110,
+    img: '/images/southindian/plain-dosa.jpeg',
+    quantity: 1
+},
+{
+    id: 24,
+    name: 'Rava Uttapam',
+    category : 'southindian',
+    price: 75,
+    img: '/images/southindian/Rava-Uttapam.jpg',
+    quantity: 1
+},
+{
+    id: 25,
+    name: 'Sambhar Vada',
+    category : 'southindian',
+    price: 55,
+    img: '/images/southindian/sambhar-vada.jpg',
+    quantity: 1
+},
+
+{
+    id: 26,
+    name: 'Apple Juice',
+    category : 'juice',
+    price: 100,
+    img: '/images/juice/apple-juice.jpg',
+    quantity: 1
+},
+{
+    id: 27,
+    name: 'Mango Juice',
+    category : 'juice',
+    price: 120,
+    img: '/images/juice/Mango-Juice.jpg',
+    quantity: 1
+},
+{
+    id: 28,
+    name: 'Passionfruit Juice',
+    category : 'juice',
+    price: 130,
+    img: '/images/juice/passionfruit-juice.jpg',
+    quantity: 1
+},
+{
+    id: 29,
+    name: 'PIneapple Juice',
+    category : 'juice',
+    price: 95,
+    img: '/images/juice/PIneapple-Juice.png',
+    quantity: 1
+},
+{
+    id: 30,
+    name: 'Strawberry Juice',
+    category : 'juice',
+    price: 125,
+    img: '/images/juice/strawberry-juice.jpg',
+    quantity: 1
+},
+]
+
+export {foodItem};
